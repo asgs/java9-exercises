3915ba8f0cbceeee6a0970d84b3999c965538691
asgs <asgosa.eeegct@gmail.com>
2017-10-06 12:32:52 +0530

    added sync-version of making a GET call.

--- src/main/java/org/asgs/exercises/java9/http2/http/HttpClientApp.java
+++ src/main/java/org/asgs/exercises/java9/http2/http/HttpClientApp.java
@@ -4,20 +4,39 @@ import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
 
+import java.io.IOException;
 import java.net.URI;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpClientApp {
+  private static ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-  public static void main(String[] args) {
-    HttpClient httpClient = HttpClient.newHttpClient();
-    HttpRequest httpRequest =
-        HttpRequest.newBuilder()
-            .GET()
-            .uri(URI.create("http://www.google.co.in?q=hello+google"))
+  public static void main(String[] args)
+      throws ExecutionException, InterruptedException, IOException {
+    HttpClientApp httpClientApp = new HttpClientApp();
+    httpClientApp.makeGetCall();
+    executorService.shutdown();
+  }
+
+  private void makeGetCall() throws IOException, InterruptedException {
+    HttpClient httpClient =
+        HttpClient.newBuilder()
+            .executor(executorService)
+            .followRedirects(HttpClient.Redirect.SECURE)
             .build();
-    CompletableFuture<HttpResponse<String>> future =
-        httpClient.sendAsync(httpRequest, HttpResponse.BodyHandler.asString());
-    future.thenAccept(System.out::println).thenRun(() -> System.out.println("Done!"));
+    HttpRequest httpRequest =
+        HttpRequest.newBuilder().GET().uri(URI.create("http://asgs.tech")).build();
+    HttpResponse<String> httpResponse =
+        httpClient.send(httpRequest, HttpResponse.BodyHandler.asString());
+    dumpResponse(httpResponse);
+  }
+
+  private <T> void dumpResponse(HttpResponse<T> httpResponse) {
+    System.out.println(
+        "Status Line is " + httpResponse.statusCode() + " " + httpResponse.version());
+    System.out.println("Headers are " + httpResponse.headers().map());
+    System.out.println("Body is " + httpResponse.body());
   }
 }
